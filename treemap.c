76e05055c2650e443e355b086f6f4afe75b682dd
repl.it user <replituser@example.com>
2020-10-02 20:24:20 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -44,7 +44,9 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
 
 
 void insertTreeMap(TreeMap * tree, void* key, void * value) {
+  if(searchTreeMap(tree,key) == NULL){
 
+  }
 }
 
 TreeNode * minimum(TreeNode * x){
