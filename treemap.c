0b2580a017c3429304065a67964d0abb535b054b
repl.it user <replituser@example.com>
2020-10-02 20:31:10 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -45,7 +45,20 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
 
 void insertTreeMap(TreeMap * tree, void* key, void * value) {
   if(searchTreeMap(tree,key) == NULL){
-
+    tree->current = tree->root;
+    if(tree->current->key == NULL){
+      tree->current = createTreeNode(key,value);
+    }
+    else{
+      if(((*(int*)key) > (*(int*)(tree->current->key))) && (tree->current->right != NULL)){
+        tree->current = tree->current->right;
+      }
+      else{
+        if(((*(int*)key) < (*(int*)(tree->current->key))) && (tree->current->left != NULL)){
+          tree->current = tree->current->left;
+        }
+      }
+    }
   }
 }
 
